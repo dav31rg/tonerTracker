02289da2b23618a8275c45c8adeb644c1ec8a3d6
Davel <davel.reymundo@gmail.com>
2023-09-19 16:15:37 -0500

    add csv

--- toner_tracker.rb
+++ toner_tracker.rb
@@ -2,26 +2,38 @@
 
 require 'nokogiri'
 require 'open-uri'
+require 'csv'
+require 'date'
 
 # list of webs
-webs = [
+printer_webs = [
   '/mnt/c/prueba-web/test-web/prueba.html',
   '/mnt/c/prueba-web/test-web/prueba1.html',
   '/mnt/c/prueba-web/test-web/prueba2.html',
   '/mnt/c/prueba-web/test-web/prueba3.html'
 ]
 
-webs.each_with_index do |web, index|
-  # puts "Analizando pagina web #{index + 1}: #{web}"
-  printer_name = File.basename(web, '.html')
-  html = open(web)
-  doc = Nokogiri::HTML(html)
-  toner = doc.at_css('li#TonerSupplies div span.dataText:first-child').text
-  drum = doc.at_css('li#PCDrumStatus div.progress-slider span.dataText').text
-  fuser = doc.at_css('li#FuserSuppliesStatus div.progress-slider span.dataText').text
+# CSV archive
+csv_archive = 'data.csv'
+today = Date.today
 
-  puts "Datos para: #{index + 1} - #{printer_name}:"
-  puts "El % de toner es: #{toner}"
-  puts "El % de drum es: #{drum}"
-  puts "El % de kit de mantenimiento es: #{fuser}"
+printer_webs.each do |printer_web|
+  printer_name = File.basename(printer_web, '.html')
+
+  begin
+    html = open(printer_web)
+    doc = Nokogiri::HTML(html)
+    toner = doc.at_css('li#TonerSupplies div span.dataText:first-child').text
+    drum = doc.at_css('li#PCDrumStatus div.progress-slider span.dataText').text
+    fuser = doc.at_css('li#FuserSuppliesStatus div.progress-slider span.dataText').text
+    new_data = [today, printer_name, toner, drum, fuser]
+  rescue OpenURI::HTTPError
+    new_data = [today, printer_name, 'no available', 'no available', 'no available']
+  end
+
+  CSV.open(csv_archive, 'a') do |csv|
+    csv << new_data
+  end
+
+  puts new_data.inspect
 end
